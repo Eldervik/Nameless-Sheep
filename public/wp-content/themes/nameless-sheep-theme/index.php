fe1c4ff3985633c5df8c50026f820f11b491204b
unknown <michal10203040@gmail.com>
2019-12-05 10:36:54 +0100

    new theme

---
+++ public/wp-content/themes/nameless-sheep-theme/index.php
@@ -0,0 +1,9 @@
+<?php include_once('header.php'); ?>
+    <main class="d-flex flex-row-reverse">
+        <?php get_template_part( 'global-templates/sidebar' ); ?>
+        <div class="col-9">
+            index
+        </div>
+        <?php get_template_part( 'global-templates/chat' ); ?>
+    </main>
+<?php include_once('footer.php'); ?>
