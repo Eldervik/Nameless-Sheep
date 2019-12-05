49a38f9df3bd16a396e2b8da7642d94818c8b1fc
Michal K <michal10203040@gmail.com>
2019-12-05 11:39:41 +0100

    Added bootstrap collapse to sidebar

--- public/wp-content/themes/nameless-sheep-theme/global-templates/sidebar.php
+++ public/wp-content/themes/nameless-sheep-theme/global-templates/sidebar.php
@@ -1,3 +1,51 @@
-<div id="sidebar">
-    sidebar
+<div id="sidebar" class="col-3 p-0">
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseTwo">
+                        Category #1
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body d-flex flex-column">
+                    <a href="#">Sub Category #1</a>
+                    <a href="#">Sub Category #2</a>
+                    <a href="#">Sub Category #3</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Category #2
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body d-flex flex-column">
+                    <a href="#">Sub Category #1</a>
+                    <a href="#">Sub Category #2</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Category #3
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body d-flex flex-column">
+                    <a href="#">Sub Category #1</a>
+                    <a href="#">Sub Category #2</a>
+                    <a href="#">Sub Category #3</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
