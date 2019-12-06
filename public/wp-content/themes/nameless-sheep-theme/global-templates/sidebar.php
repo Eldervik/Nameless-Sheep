d916a10ac40496dfbb644226bba1dd8a8ea0902d
Michal K <michal10203040@gmail.com>
2019-12-06 13:14:32 +0100

    Google fonts, animations

--- public/wp-content/themes/nameless-sheep-theme/global-templates/sidebar.php
+++ public/wp-content/themes/nameless-sheep-theme/global-templates/sidebar.php
@@ -1,57 +1,59 @@
 <div id="sidebar" class="col-3 p-0 d-flex flex-column">
     <form>
-        <input type="text" name="Search">
+        <input type="text" name="Search" placeholder="Search Product...">
     </form>
     <div class="accordion flex-grow-1" id="accordionExample">
         <div class="card">
             <div class="card-header" id="headingTwo">
                 <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseTwo">
+                    <button class="btn btn-link collapsed p-0" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseTwo">
                         <i class="fas fa-apple-alt"></i><span>Category #1</span>
                     </button>
                 </h2>
             </div>
             <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                <div class="card-body d-flex flex-column">
-                    <a href="#">Sub Category #1</a>
-                    <a href="#">Sub Category #2</a>
-                    <a href="#">Sub Category #3</a>
+                <div class="d-flex flex-column">
+                    <a href="#" class="card-header">Sub Category #1</a>
+                    <a href="#" class="card-header">Sub Category #2</a>
+                    <a href="#" class="card-header">Sub Category #3</a>
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-header" id="headingTwo">
                 <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <button class="btn btn-link collapsed p-0" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                         <i class="fas fa-apple-alt"></i><span>Category #2</span>
                     </button>
                 </h2>
             </div>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                <div class="card-body d-flex flex-column">
-                    <a href="#">Sub Category #1</a>
-                    <a href="#">Sub Category #2</a>
+                <div class="d-flex flex-column">
+                    <a href="#" class="card-header">Sub Category #1</a>
+                    <a href="#" class="card-header">Sub Category #2</a>
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-header" id="headingThree">
                 <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <button class="btn btn-link collapsed p-0" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                     <i class="fas fa-apple-alt"></i><span>Category #3</span>
                     </button>
                 </h2>
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                <div class="card-body d-flex flex-column">
-                    <a href="#">Sub Category #1</a>
-                    <a href="#">Sub Category #2</a>
-                    <a href="#">Sub Category #3</a>
+                <div class="d-flex flex-column">
+                    <a href="#" class="card-header">Sub Category #1</a>
+                    <a href="#" class="card-header">Sub Category #2</a>
+                    <a href="#" class="card-header">Sub Category #3</a>
                 </div>
             </div>
         </div>
     </div>
-    <div class="socials">
+    <div class="socials d-flex">
+        <i class="fab fa-facebook-square"></i>
+        <i class="fab fa-facebook-square"></i>
         <i class="fab fa-facebook-square"></i>
     </div>
 </div>
