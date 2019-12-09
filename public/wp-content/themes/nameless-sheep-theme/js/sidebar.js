9bbac71a623fd7d6469fe5da5d52b45ffbb68ba7
Michal K <michal10203040@gmail.com>
2019-12-09 18:50:44 +0100

    Hide

--- public/wp-content/themes/nameless-sheep-theme/js/sidebar.js
+++ public/wp-content/themes/nameless-sheep-theme/js/sidebar.js
@@ -1,8 +1,9 @@
 let open = true;
-let test = (100 / 12) * 3;
+let col = (100 / 12) * 3;
+let width = $(window).width();
 $( '#arrow' ).click(function() {
     if (open == true) {
-        $('#sidebar').css( 'left', '-' + test + '%' );
+        $('#sidebar').css( 'left', '-' + col + '%' );
         $('#arrow i').css( 'transform', 'rotate(180deg)' );
         open = false;
     } else {
@@ -10,4 +11,21 @@ $( '#arrow' ).click(function() {
         $('#arrow i').css( 'transform', 'rotate(0deg)' );
         open = true;
     }
+});
+
+function hideOnResize() {
+    if (width <= 1000) {
+        $('#sidebar').css( 'left', '-' + col + '%' );
+        $('#arrow i').css( 'transform', 'rotate(180deg)' );
+        open = false;
+    } else {
+        $('#sidebar').css( 'left', '0' );
+        $('#arrow i').css( 'transform', 'rotate(0deg)' );
+        open = true;
+    }
+};
+
+$(window).resize(function() {
+    width = $(window).width();
+    hideOnResize();
 });
