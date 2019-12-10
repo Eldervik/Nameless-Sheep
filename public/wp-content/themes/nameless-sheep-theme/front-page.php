ef3cb8b0aad5ebe96c67d710a68d0d3be41dffb7
Michal K <michal10203040@gmail.com>
2019-12-10 14:35:24 +0100

    Ready for code

--- public/wp-content/themes/nameless-sheep-theme/front-page.php
+++ public/wp-content/themes/nameless-sheep-theme/front-page.php
@@ -1,10 +1,10 @@
 <?php include_once('header.php'); ?>
     <main class="d-flex flex-row-reverse">
         <?php get_template_part( 'global-templates/sidebar' ); ?>
-        <div class="col-9">
-            index
+        <div class="content col-9">
+            front-page
+            <?php get_template_part( 'global-templates/chat' ); ?>
         </div>
-        <?php get_template_part( 'global-templates/chat' ); ?>
     </main>
 <?php include_once('footer.php'); ?>
 
--- public/wp-content/themes/nameless-sheep-theme/page.php
+++ public/wp-content/themes/nameless-sheep-theme/page.php
@@ -1,14 +1,14 @@
 <?php include_once('header.php'); ?>
-<h1><?php the_title();?></h1>
 <main class="d-flex flex-row-reverse">
     <?php get_template_part( 'global-templates/sidebar' ); ?>
-    <div class="col-9">
-        index
+    <div class="content col-9">
+        <h1><?php the_title();?></h1>
+        page
+        <?php get_template_part( 'global-templates/chat' ); ?>
+        <img src="<?php the_post_thumbnail_url('post_image');?>">
+        <?php if(have_posts()) : while(have_posts()) : the_post();?>
+            <?php the_content();?>
+        <?php endwhile; else: endif; ?>
     </div>
-    <?php get_template_part( 'global-templates/chat' ); ?>
-    <img src="<?php the_post_thumbnail_url('post_image');?>">
-    <?php if(have_posts()) : while(have_posts()) : the_post();?>
-        <?php the_content();?>
-    <?php endwhile; else: endif; ?>
 </main>
 <?php include_once('footer.php'); ?>
