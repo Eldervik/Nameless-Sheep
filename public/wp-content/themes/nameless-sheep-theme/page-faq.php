c34b56d8ece72a4b7d465f285623ce66d9a3e1ea
Prattay <pcdaking@yahoo.com>
2019-12-10 15:19:43 +0100

    Prattay och Sargon gjort FAQ.
    Add Faq page, according to wp hiearchy, add bootstrap accordion.

--- public/wp-content/themes/nameless-sheep-theme/page-faq.php
+++ public/wp-content/themes/nameless-sheep-theme/page-faq.php
@@ -2,7 +2,7 @@
     <main class="d-flex flex-row-reverse">
         <?php get_template_part( 'global-templates/sidebar' ); ?>
         <div class="content col-9">
-             
+             <?php include_once('global-templates/single_faq.php'); ?>
         </div>
         <?php get_template_part( 'global-templates/chat' ); ?>
     </main>
--- public/wp-content/themes/nameless-sheep-theme/single.php
+++ public/wp-content/themes/nameless-sheep-theme/single.php
@@ -1,3 +1,4 @@
 <?php include_once('header.php'); ?>
-<?php include_once('loop-templates/content-single.php'); ?>
+
 <?php include_once('footer.php'); ?>
+<?php include_once('global-templates/single_faq.php'); ?>
