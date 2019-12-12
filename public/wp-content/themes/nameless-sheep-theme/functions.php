63c8e897232355b50ec9cbd3ebb05d65a2e8801d
Unknown <nariman.alkurdi@hotmail.com>
2019-12-12 09:31:41 +0100

    add widget to our theme

--- public/wp-content/themes/nameless-sheep-theme/functions.php
+++ public/wp-content/themes/nameless-sheep-theme/functions.php
@@ -31,3 +31,23 @@ function my_custom_currency_symbol( $symbol ){
 	
     return $symbol;
 }
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Home right sidebar',
+		'id'            => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+?>
