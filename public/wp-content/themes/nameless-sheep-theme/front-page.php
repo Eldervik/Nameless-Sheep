a0d7a570e94fbb74563ac0b9814851dc2e4c5011
Anthon Eldervik <Anthon@Eldervik.se>
2019-12-16 14:24:16 +0100

    Added quantity field for shop page

--- public/wp-content/themes/nameless-sheep-theme/front-page.php
+++ public/wp-content/themes/nameless-sheep-theme/front-page.php
@@ -3,6 +3,7 @@
         <?php get_template_part( 'global-templates/sidebar' ); ?>
         <div class="content col-9">
             front-page
+            <?php dynamic_sidebar('main-bar'); ?>
             <?php get_template_part( 'global-templates/chat' ); ?>
         </div>
     </main>
