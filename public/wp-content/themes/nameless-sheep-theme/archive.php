b25a928b4df3e79607a95aa2eede09c0f00b3202
Michal K <michal10203040@gmail.com>
2019-12-17 11:36:17 +0100

    Loop products

--- public/wp-content/themes/nameless-sheep-theme/archive.php
+++ public/wp-content/themes/nameless-sheep-theme/archive.php
@@ -1,1 +1,5 @@
-archive
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    <img src="<?php the_post_thumbnail_url('post_image'); ?>" alt="<?php the_title(); ?>">
+    <?php the_excerpt(); ?>
+<?php endwhile; else: endif; ?>
--- public/wp-content/themes/nameless-sheep-theme/page.php
+++ public/wp-content/themes/nameless-sheep-theme/page.php
@@ -5,10 +5,16 @@
         <h1><?php the_title();?></h1>
         page
         <?php get_template_part( 'global-templates/chat' ); ?>
-        <img src="<?php the_post_thumbnail_url('post_image');?>">
         <?php if(have_posts()) : while(have_posts()) : the_post();?>
             <?php the_content();?>
         <?php endwhile; else: endif; ?>
     </div>
 </main>
 <?php include_once('footer.php'); ?>
+
+
+<style>
+.product {
+    box-shadow: 0 0 5px black;
+}
+</style>
