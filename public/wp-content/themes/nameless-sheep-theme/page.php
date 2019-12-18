ee036a1d7c26fc25f9bae91fa60c19ffb0eac1b3
Michal K <michal10203040@gmail.com>
2019-12-18 13:46:13 +0100

    yes

--- public/wp-content/themes/nameless-sheep-theme/page.php
+++ public/wp-content/themes/nameless-sheep-theme/page.php
@@ -8,6 +8,27 @@
         <?php if(have_posts()) : while(have_posts()) : the_post();?>
             <?php the_content();?>
         <?php endwhile; else: endif; ?>
+
+
+        <ul class="products">
+                <?php
+                    $args = array(
+                        'post_type' => 'product',
+                        'posts_per_page' => 12
+                        );
+                    $loop = new WP_Query( $args );
+                    if ( $loop->have_posts() ) {
+                        while ( $loop->have_posts() ) : $loop->the_post();
+                            wc_get_template_part( 'content', 'product' );
+                        endwhile;
+                    } else {
+                        echo __( 'No products found' );
+                    }
+                    wp_reset_postdata();
+                ?>
+            </ul><!--/.products-->
+
+
     </div>
 </main>
 <?php include_once('footer.php'); ?>
--- public/wp-content/themes/nameless-sheep-theme/search.php
+++ public/wp-content/themes/nameless-sheep-theme/search.php
@@ -3,6 +3,25 @@
         <?php get_template_part( 'global-templates/sidebar' ); ?>
         <div class="content col-9">
             <h1><?php esc_html_e( 'Search result', 'nameless-sheep' ); ?></h1>
+            
+            <ul class="products">
+                <?php
+                    $args = array(
+                        'post_type' => 'product',
+                        'posts_per_page' => 12
+                        );
+                    $loop = new WP_Query( $args );
+                    if ( $loop->have_posts() ) {
+                        while ( $loop->have_posts() ) : $loop->the_post();
+                            wc_get_template_part( 'content', 'product' );
+                        endwhile;
+                    } else {
+                        echo __( 'No products found' );
+                    }
+                    wp_reset_postdata();
+                ?>
+            </ul><!--/.products-->
+
             <?php get_template_part( 'global-templates/chat' ); ?>
         </div>
     </main>
