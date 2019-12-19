5febe1989246b18b9e6a7b5d1bdf7cbe1b6cdca0
Anthon Eldervik <Anthon@Eldervik.se>
2019-12-19 12:58:51 +0100

    Added wishlist plugin

--- public/wp-content/plugins/ns-facebook/inc/widgets.php
+++ public/wp-content/plugins/ns-facebook/inc/widgets.php
@@ -2,7 +2,7 @@
 
 require("class.nsfacebook.php");
 
-function wrp_widgets_init() {
+function nsfb_widgets_init() {
 	register_widget('nsfacebook');
 }
-add_action('widgets_init', 'wrp_widgets_init');
+add_action('widgets_init', 'nsfb_widgets_init');
